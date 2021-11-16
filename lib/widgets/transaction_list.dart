1b2422fc237eadf2a189342042f2df2fa06979de
kingsmen3 <90978041+kingsmen3@users.noreply.github.com>
2021-11-16 19:13:43 +0530

    unchanged

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,51 +9,54 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transaction.map((tx) {
-        return Card(
-          //color: Colors.deepOrange,
-          child: Row(
-            children: [
-              Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2,
-                      )),
-                  padding: EdgeInsets.all(20),
-                  margin: EdgeInsets.symmetric(
-                      vertical: 10, horizontal: 15),
-                  child: Text(
-                    '\$${tx.amount}',
-                    style: TextStyle(
-                      color: Colors.purple,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  )),
-              Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      tx.title,
+    return Container(
+      height: 300,
+      child: Column(
+        children: transaction.map((tx) {
+          return Card(
+            //color: Colors.deepOrange,
+            child: Row(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        )),
+                    padding: EdgeInsets.all(20),
+                    margin: EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 15),
+                    child: Text(
+                      '\$${tx.amount}',
                       style: TextStyle(
+                        color: Colors.purple,
                         fontWeight: FontWeight.bold,
-                        fontSize: 16,
+                        fontSize: 20,
                       ),
-                    ),
-                    Text(
-                      DateFormat.yMMMEd().format(tx.date),
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        );
-      }).toList(),
+                    )),
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        DateFormat.yMMMEd().format(tx.date),
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
 }
