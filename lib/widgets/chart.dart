1b4d3b598ea91ab1a8b6aa66b570b75c6da73b29
kingsmen3 <90978041+kingsmen3@users.noreply.github.com>
2021-11-28 12:26:15 +0530

    revised

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -5,25 +5,21 @@ import 'package:udemy/models/transaction.dart';
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
 
-  Chart({this.recentTransactions});
+  const Chart({Key key, this.recentTransactions}) : super(key: key);
 
-  List<Map<String, Object>> get groupedTransactionValues {
+  List<Map<String, Object>> get goupedTransactoinValues {
     return List.generate(7, (index) {
-      final weekDay = DateTime.now().subtract(Duration(days: index));
+      final weekday = DateTime.now().subtract(Duration(days: index));
       var totalSum = 0.0;
 
       for (var i = 0; i < recentTransactions.length; i++) {
-        if (recentTransactions[i].date.day == weekDay.day &&
-            recentTransactions[i].date.month == weekDay.month &&
-            recentTransactions[i].date.year == weekDay.year) {
+        if (recentTransactions[i].date.day == weekday.day &&
+            recentTransactions[i].date.month == weekday.month &&
+            recentTransactions[i].date.year == weekday.year) {
           totalSum += recentTransactions[i].amount;
         }
       }
-
-      print(DateFormat.E(weekDay));
-      print(totalSum);
-
-      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+      return {'day': DateFormat.E(weekday), 'amount': totalSum};
     });
   }
 
